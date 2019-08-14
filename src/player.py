096818af4228812a3cfaeed20f64f7251c9a8972
Jonathan Dorety <kyullog@gmail.com>
2019-08-13 20:24:24 -0700

    Add items

--- src/player.py
+++ src/player.py
@@ -3,9 +3,14 @@
 
 
 class Player:
-    def __init__(self, name, location):
+    def __init__(self, name, location, inventory):
         self.name = name
         self.location = location
+        self.inventory = inventory
 
     def change_location(self, new_location):
         self.location = new_location
+
+    def take_item(self, item):
+        self.location.rem_item(item)
+        self.inventory.append(item)
--- src/room.py
+++ src/room.py
@@ -3,9 +3,19 @@
 
 
 class Room:
-    def __init__(self, name, description):
+    def __init__(self, name, description, items=[]):
         self.name = name
         self.description = description
+        self.items = items
+
+    def rem_item(self, item):
+        try:
+            self.items.remove(item)
+        except(ValueError):
+            print("Item does not exist")
+
+    def add_item(self, item):
+        self.items.append(item)
 
 
 
